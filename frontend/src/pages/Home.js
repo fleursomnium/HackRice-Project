7c232a5588ec994f4caa9c4585866791f21a6458
Miguel Rodriguez <wicketmiguelr@gmail.com>
2024-09-21 19:15:12 -0500

    renamed to ChronBeGone

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,12 +4,12 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      <h1 className="home-title">Welcome to TheraMove</h1>
+      <h1 className="home-title">Welcome to ChronBeGone</h1>
       <p className="home-description">
-        If you have any pains in any body parts, we can help you figure out the causes and fixes to help
+        If you have any chronic pains in any body parts, we can help you figure out the causes and fixes to help
         reduce or even eliminate the pain completely. Motion is medicine, the body needs to move a certain
         amount for it to function properly. We will give you the knowledge necessary to help fix your problem,
-        but it is still up to you to do what is necessary.
+        but it is still up to you to do the movements necessary.
       </p>
     </div>
   );
